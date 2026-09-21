cfb60d1f3cdf40a39bcde29edb8dff64ad538cd3
Caio Macedo <162586904+maiocacedo@users.noreply.github.com>
2026-09-21 16:17:04 -0300

    Delete 07-TarefaReactPropseHooks directory

--- 07-TarefaReactPropseHooks/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Contador } from '@/components/Contador'
-
-export default function App() {
-  return (
-    <main className="flex min-h-screen items-center justify-center p-4">
-      <Contador />
-    </main>
-  )
-}
